43941071af3dd14b019e6961d3ad70a2825ff4be
SergeyXP <s.pusthotin@gmail.com>
2023-01-21 00:53:53 +0400

    homework03

---
+++ CodeReview/src/main/java/org/gb/homework/codereview/Process.java
@@ -0,0 +1,17 @@
+package org.gb.homework.codereview;
+
+
+public class Process {
+
+    public static void processing(Object object) {
+        if (object instanceof Text) {
+            System.out.println("Текст сохранен");
+            System.out.println(((Text) object).getText());
+        } else if (object instanceof Picture) {
+            System.out.println("Картинка сохранена");
+            System.out.println(((Picture) object).getMyPicture());
+        } else {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+}
