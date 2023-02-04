31a1b030939e3de92a52ee11e33fdbe8fb2ed023
SergeyXP <s.pusthotin@gmail.com>
2023-02-04 20:09:37 +0400

    Missing    csharp/SchoolData/.vs/SchoolData/FileContentIndex/0fe74768-d2b9-491f-a177-b03f1a38e43b.vsidx Missing    csharp/SchoolData/.vs/SchoolData/FileContentIndex/acc57e79-272f-4765-906b-cd3b4ab2794e.vsidx Missing    csharp/SchoolData/.vs/SchoolData/FileContentIndex/c632d6cf-f45e-4ac7-bf4d-47073bd7eb0d.vsidx Missing    csharp/SchoolData/.vs/SchoolData/FileContentIndex/ed2c786a-a7a3-4be1-9369-c546a1a15bcb.vsidx Modified   csharp/SchoolData/.vs/SchoolData/v17/.suo Modified   csharp/SchoolData/View/Tools/ConsoleHelper.cs Modified   csharp/SchoolData/obj/Debug/net6.0/SchoolData.csproj.AssemblyReference.cache Modified   csharp/SchoolData/obj/SchoolData.csproj.nuget.dgspec.json Modified   csharp/SchoolData/obj/project.assets.json Modified   csharp/SchoolData/obj/project.nuget.cache

--- csharp/SchoolData/View/Tools/ConsoleHelper.cs
+++ csharp/SchoolData/View/Tools/ConsoleHelper.cs
@@ -27,74 +27,75 @@
         }
 
         public static string getString(string prompt)
-        {
+
+
             System.out.print(prompt);
 
             return scannerIn.nextLine();
         }
 
-        public static string GetString(string prompt, Predicate<string> fun)
+    public static string GetString(string prompt, Predicate<string> fun)
+    {
+        while (true)
         {
-            while (true)
+            System.out.print(prompt);
+            var s = scannerIn.nextLine();
+
+            if (fun.test(s))
             {
-                System.out.print(prompt);
-                var s = scannerIn.nextLine();
-
-                if (fun.test(s))
-                {
-                    return s;
-                }
-
-                System.out.print("Ошибка! ");
+                return s;
             }
-        }
 
-        private static int getFromConsole(string prompt, Predicate<int> fun)
-        {
-            while (true)
-            {
-                Console.WriteLine(prompt);
-                try
-                {
-                    var n = int.Parse(Console.ReadLine() ?? "0");
-
-                    if (fun(n))
-                    {
-                        return n;
-                    }
-                }
-                catch (FormatException)
-                {
-                }
-
-                Console.WriteLine("Ошибка! ");
-            }
-        }
-
-        private static int GetFromConsole(string prompt, Predicate<int> fun, int defaultValue)
-        {
-            while (true)
-            {
-                Console.WriteLine(prompt);
-                try
-                {
-                    string? s = Console.ReadLine();
-
-                    if (string.IsNullOrEmpty(s))
-                        return defaultValue;
-
-                    var n = int.Parse(s);
-
-                    if (fun(n))
-                    {
-                        return n;
-                    }
-                }
-                catch (FormatException)
-                {
-                }
-                Console.WriteLine("Ошибка! ");
-            }
+            System.out.print("Ошибка! ");
         }
     }
+
+    private static int getFromConsole(string prompt, Predicate<int> fun)
+    {
+        while (true)
+        {
+            Console.WriteLine(prompt);
+            try
+            {
+                var n = int.Parse(Console.ReadLine() ?? "0");
+
+                if (fun(n))
+                {
+                    return n;
+                }
+            }
+            catch (FormatException)
+            {
+            }
+
+            Console.WriteLine("Ошибка! ");
+        }
+    }
+
+    private static int GetFromConsole(string prompt, Predicate<int> fun, int defaultValue)
+    {
+        while (true)
+        {
+            Console.WriteLine(prompt);
+            try
+            {
+                string? s = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(s))
+                    return defaultValue;
+
+                var n = int.Parse(s);
+
+                if (fun(n))
+                {
+                    return n;
+                }
+            }
+            catch (FormatException)
+            {
+            }
+            Console.WriteLine("Ошибка! ");
+        }
+    }
+}
 }
